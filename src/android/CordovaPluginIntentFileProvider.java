2c1a32289c568a9b549046efd88f22e2565b0b06
G Dallo <geniltodallo@gmail.com>
2023-06-20 15:54:22 -0300

    Update CordovaPluginIntentFileProvider.java

--- src/android/CordovaPluginIntentFileProvider.java
+++ src/android/CordovaPluginIntentFileProvider.java
@@ -1,4 +1,4 @@
-package com.darryncampbell.cordova.plugin.intent;
+package gdallo.cordova.plugin.intent;
 
 public class CordovaPluginIntentFileProvider extends androidx.core.content.FileProvider {
 }
